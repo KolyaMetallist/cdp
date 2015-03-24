fae957bb0ae09e95c1fb12b52bb54ea25121d93d
KolyaMetallist <kolya_metallist@mail.ru>
2015-03-24 14:56:06 +0200

    remove old package

--- Java8Test/src/main/java/com/app/Java8Test/processor/AbstractApproachTaskFactory.java
+++
@@ -1,9 +0,0 @@
-package com.app.Java8Test.processor;
-
-public class ApproachFactory {
-	
-	public static Approach getApproach(String type) {
-		return type.equals("Java8") ? new Java8Approach() : null;
-	}
-
-}
--- Java8Test/src/main/java/com/app/Java8Test/processor/Approach.java
+++
@@ -1,9 +0,0 @@
-package com.app.Java8Test.processor;
-
-import java.io.File;
-
-public interface Approach {
-	
-	void frequenceTask(File file, boolean parallel);
-
-}
