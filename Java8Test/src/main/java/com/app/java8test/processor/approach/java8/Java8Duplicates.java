59f441172fd1bc927d288b20e6f55b7a89cd26ed
KolyaMetallist <kolya_metallist@mail.ru>
2015-03-25 19:56:39 +0200

    java8 duplicates task

--- Java8Test/src/main/java/com/app/java8test/processor/approach/java8/Java8Duplicates.java
+++ Java8Test/src/main/java/com/app/java8test/processor/approach/java8/Java8Duplicates.java
@@ -1,8 +1,15 @@
 package com.app.java8test.processor.approach.java8;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 
 public class Java8Duplicates implements Java8Approach {
 
@@ -10,10 +17,31 @@ public class Java8Duplicates implements Java8Approach {
 	public void taskExecution(File file, boolean parallel, PrintStream printer) {
 		try {
 			if (parallel) {
-				
+				Collections.synchronizedList(this.readWordsFromText(file, parallel))
+					.parallelStream() // Stream<String>
+					.collect(groupingBy(e -> e, LinkedHashMap::new, counting())) // convert to Map of words and their frequency
+					.entrySet() // extract entrySet from Map
+					.parallelStream() // Stream<Entry<String, Long>>
+					.filter(e -> e.getValue() > 1) // filter all duplicates
+					.limit(3) // truncate top 3 entries
+					.map(Entry::getKey) // extract the list of duplicates
+					.map(String::toUpperCase) // convert to upper case
+					.map(e -> {return new StringBuffer(e).reverse().toString();}) // inverse
+					.sorted(comparing(String::length)) // sort by length ascending
+					.forEachOrdered(printer::println);
 			} else {
 				this.readWordsFromText(file, parallel)
-					.stream(); // Stream<String>
+					.stream() // Stream<String>
+					.collect(groupingBy(e -> e, LinkedHashMap::new, counting())) // convert to Map of words and their frequency
+					.entrySet() // extract entrySet from Map
+					.stream() // Stream<Entry<String, Long>>
+					.filter(e -> e.getValue() > 1) // filter all duplicates
+					.limit(3) // truncate top 3 entries
+					.map(Entry::getKey) // extract the list of duplicates
+					.map(String::toUpperCase) // convert to upper case
+					.map(e -> {return new StringBuilder(e).reverse().toString();}) // inverse
+					.sorted(comparing(String::length)) // sort by length ascending
+					.forEach(printer::println);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
