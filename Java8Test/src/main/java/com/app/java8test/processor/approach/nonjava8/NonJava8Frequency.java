e3999acfb7485015595a53b32c113d299b00f604
KolyaMetallist <kolya_metallist@mail.ru>
2015-03-26 00:53:24 +0200

    add NonJava8 frequency task

--- Java8Test/src/main/java/com/app/java8test/processor/approach/nonjava8/NonJava8Frequency.java
+++ Java8Test/src/main/java/com/app/java8test/processor/approach/nonjava8/NonJava8Frequency.java
@@ -3,14 +3,60 @@ package com.app.java8test.processor.approach.nonjava8;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class NonJava8Frequency implements NonJava8Approach {
 
 	@Override
 	public void taskExecution(File file, boolean parallel, PrintStream printer) {
-		// TODO Auto-generated method stub
-		
-	}
+		try {
+			Map<String, Integer> wordsFrequency = new HashMap<>();
+			for(String word : this.readWordsFromText(file, parallel)) {
+				if (wordsFrequency.containsKey(word)) {
+					wordsFrequency.put(word, wordsFrequency.get(word).intValue() + 1);
+				} else {
+					wordsFrequency.put(word, 1);
+				}
+			}
+			
+			List<Entry<String, Integer>> entryList = new ArrayList<>();
+			entryList.addAll(wordsFrequency.entrySet());
+			
+			Collections.sort(entryList, new Comparator<Entry<String, Integer>>(){
 
+				@Override
+				public int compare(Entry<String, Integer> o1,
+						Entry<String, Integer> o2) {
+					return o2.getValue().compareTo(o1.getValue());
+				}
+				
+			});
+			
+			int topIndex = entryList.size() > 1 ? 2 : entryList.size() > 0 ? 1 : 0;
+			
+			entryList = entryList.subList(0, topIndex);
+			
+			Collections.sort(entryList, new Comparator<Entry<String, Integer>>(){
+
+				@Override
+				public int compare(Entry<String, Integer> o1,
+						Entry<String, Integer> o2) {
+					return o2.getKey().compareTo(o1.getKey());
+				}
+				
+			});
+			
+			for(Entry<String, Integer> entry : entryList) {
+				System.out.println(entry);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
