53de49b311d6855d5f45c4744663d3b5809d84af
Mykola Bazhenov <mykola_bazhenov@epam.com>
2015-04-03 15:42:37 +0300

    add non java 8 approach test

---
+++ Java8Test/src/test/java/com/app/java8test/processor/approach/nonjava8/NonJava8ApproachTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.app.java8test.processor.approach.nonjava8;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Mykola_Bazhenov
+ *
+ */
+public class NonJava8ApproachTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.app.java8test.processor.approach.nonjava8.NonJava8Approach#readWordsFromText(java.io.File, boolean)}.
+	 */
+	@Test
+	public void testReadWordsFromText() {
+		//fail("Not yet implemented");
+	}
+
+}
