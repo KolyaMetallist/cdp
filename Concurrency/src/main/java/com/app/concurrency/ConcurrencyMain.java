65f0123966e0525fda437144505430ad1c4b8ae0
KolyaMetallist <kolya_metallist@mail.ru>
2015-04-06 00:02:31 +0300

    add Concurrency project

---
+++ Concurrency/src/main/java/com/app/concurrency/ConcurrencyMain.java
@@ -0,0 +1,13 @@
+package com.app.concurrency;
+
+/**
+ * Hello world!
+ *
+ */
+public class ConcurrencyMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
