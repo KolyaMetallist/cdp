c16f095d802b30c781bcd21e81787609a1df9316
Mykola Bazhenov <mykola_bazhenov@epam.com>
2015-04-14 11:16:04 +0300

    change threshold

--- Concurrency/src/main/java/com/app/concurrency/ConcurrencyMain.java
+++ Concurrency/src/main/java/com/app/concurrency/ConcurrencyMain.java
@@ -12,57 +12,61 @@ public class ConcurrencyMain {
 	/**
 	 * From this reference {@link https://antimatroid.wordpress.com/2012/12/01/parallel-merge-sort-in-java/}
 	 * it was concluded that the threshold for sequential merge is more effective than parallel 
-	 * when size > 1024 array items
+	 * when size < 1024 array items ???
 	 */
-	private static final int THRESHOLD = 1024;
+	private static final int THRESHOLD = 500000;
 	
     public static void main( String[] args ) throws InterruptedException {
     	
-    	final int arraySize = 8192000;
+    	int arraySize = 10000;
     	
+    	for(int i = 0; i < 10; i++) {
     	
-    	int values[] = createRandomArray(arraySize);
-    	int values2[] = values.clone();
-    	int values3[] = values.clone();
-    	
-    	System.out.println("Array size = " + values.length);
-    	
-    	//*************************************************
-    	// Single thread sorting
-    	//Arrays.stream(values).forEach(a -> System.out.print(a + " "));
-    	long tStart = System.currentTimeMillis();
-    	SingleMergeSort singleSorter = new SingleMergeSort(values);
-    	singleSorter.sort();
-    	long elapsedTime = System.currentTimeMillis() - tStart;
-		
-    	System.out.println("\nAfter sorting:");
-    	//Arrays.stream(values).forEach(a -> System.out.print(a + " "));
-    	System.out.println("\nElapsed time: " + elapsedTime + " ms\n");
-    	
-    	//*************************************************
-    	// Multi thread sorting with wait/notify
-    	//Arrays.stream(values2).forEach(a -> System.out.print(a + " "));
-    	int cores = getSortThreads(arraySize);
-    	tStart = System.currentTimeMillis();
-    	Thread parallelSorter = new Thread(new ParallelMergeSort(values2, cores));
-    	parallelSorter.start();
-    	parallelSorter.join();
-    	elapsedTime = System.currentTimeMillis() - tStart;
-    	
-    	System.out.println("\nAfter parallel sorting with wait/notify synchronization:");
-    	//Arrays.stream(values2).forEach(a -> System.out.print(a + " "));
-    	System.out.println("\n" + cores + " threads. Elapsed time: " + elapsedTime + " ms\n");
-    	
-    	//*************************************************
-    	// Multi thread sorting with fork/join
-    	//Arrays.stream(values3).forEach(a -> System.out.print(a + " "));
-    	tStart = System.currentTimeMillis();
-    	ParallelMergeSort2.parallelMergeSort(values3, cores);
-    	elapsedTime = System.currentTimeMillis() - tStart;
-    	
-    	System.out.println("\nAfter parallel static sorting with a fork/join concept:");
-    	//Arrays.stream(values3).forEach(a -> System.out.print(a + " "));
-    	System.out.println("\n" + cores + " threads. Elapsed time: " + elapsedTime + " ms");
+	    	int values[] = createRandomArray(arraySize);
+	    	int values2[] = values.clone();
+	    	int values3[] = values.clone();
+	    	
+	    	System.out.println("Array size = " + values.length);
+	    	
+	    	//*************************************************
+	    	// Single thread sorting
+	    	//Arrays.stream(values).forEach(a -> System.out.print(a + " "));
+	    	long tStart = System.currentTimeMillis();
+	    	SingleMergeSort singleSorter = new SingleMergeSort(values);
+	    	singleSorter.sort();
+	    	long elapsedTime = System.currentTimeMillis() - tStart;
+			
+	    	System.out.println("\nAfter sorting:");
+	    	//Arrays.stream(values).forEach(a -> System.out.print(a + " "));
+	    	System.out.println("Elapsed time: " + elapsedTime + " ms\n");
+	    	
+	    	//*************************************************
+	    	// Multi thread sorting with wait/notify
+	    	//Arrays.stream(values2).forEach(a -> System.out.print(a + " "));
+	    	int cores = getSortThreads(arraySize);
+	    	tStart = System.currentTimeMillis();
+	    	Thread parallelSorter = new Thread(new ParallelMergeSort(values2, cores));
+	    	parallelSorter.start();
+	    	parallelSorter.join();
+	    	elapsedTime = System.currentTimeMillis() - tStart;
+	    	
+	    	System.out.println("\nAfter parallel sorting with wait/notify synchronization:");
+	    	//Arrays.stream(values2).forEach(a -> System.out.print(a + " "));
+	    	System.out.println(cores + " threads. Elapsed time: " + elapsedTime + " ms\n");
+	    	
+	    	//*************************************************
+	    	// Multi thread sorting with fork/join
+	    	//Arrays.stream(values3).forEach(a -> System.out.print(a + " "));
+	    	tStart = System.currentTimeMillis();
+	    	ParallelMergeSort2.parallelMergeSort(values3, cores);
+	    	elapsedTime = System.currentTimeMillis() - tStart;
+	    	
+	    	System.out.println("\nAfter parallel static sorting with a fork/join concept:");
+	    	//Arrays.stream(values3).forEach(a -> System.out.print(a + " "));
+	    	System.out.println(cores + " threads. Elapsed time: " + elapsedTime + " ms");
+	    	
+	    	arraySize *= 2; // Enlarge the array size
+    	}
     }
     
     /**
@@ -86,9 +90,9 @@ public class ConcurrencyMain {
      * @return the thread count
      */
     public static int getSortThreads(int length) {
-    	//int cores = length > (THRESHOLD*2) ? length / (THRESHOLD*2) : 1; 
-    	// decided to use core count. Link http://faculty.ycp.edu/~dhovemey/spring2011/cs365/lecture/lecture18.html
-    	int cores = Runtime.getRuntime().availableProcessors(); 
+    	// decided to use core count in the case of parallel approach. Link http://faculty.ycp.edu/~dhovemey/spring2011/cs365/lecture/lecture18.html
+    	int cores = length <  THRESHOLD ? 1
+    			: Runtime.getRuntime().availableProcessors(); 
     	// cores = 8;
     	return cores;
     }
