d426858fd343a40093d2b6fdbe3d6dd74fd1e813
KolyaMetallist <kolya_metallist@mail.ru>
2015-05-02 22:52:10 +0300

    add test for TicketServiceImpl

--- Spring/BasicModule/src/test/java/com/ticketbooking/app/AbstractTest.java
+++ Spring/BasicModule/src/test/java/com/ticketbooking/app/AbstractTest.java
@@ -29,6 +29,8 @@ public abstract class AbstractTest {
 	public static final String USER_NEW_NAME = "New Name";
 	public static final long USER_ID = 123l;
 	public static final long EVENT_ID = 45l;
+	public static final long TICKET_ID = 23l;
+	public static final int PLACE = 234;
 	public static final String EVENT_NAME = "Concert";
 
 	/**
--- Spring/BasicModule/src/test/java/com/ticketbooking/service/impl/EventServiceImplTest.java
+++ Spring/BasicModule/src/test/java/com/ticketbooking/service/impl/EventServiceImplTest.java
@@ -13,7 +13,6 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.junit.Assert.*;
 
 import java.text.ParseException;
 import java.util.Arrays;
