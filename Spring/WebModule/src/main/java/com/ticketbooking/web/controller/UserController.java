a19fdb2b89bcdf152d07a4979760e9510b21f8cc
Mykola Bazhenov <mykola_bazhenov@epam.com>
2015-06-04 18:05:04 +0300

    create user page

--- Spring/WebModule/src/main/java/com/ticketbooking/web/controller/UserController.java
+++ Spring/WebModule/src/main/java/com/ticketbooking/web/controller/UserController.java
@@ -1,17 +1,31 @@
 package com.ticketbooking.web.controller;
 
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ticketbooking.facade.BookingFacade;
+import com.ticketbooking.model.User;
+import com.ticketbooking.model.UserAccount;
+import com.ticketbooking.model.impl.UserImpl;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
+	
+	private static final Logger logger = LogManager.getLogger();
 	
 	@Autowired
 	private BookingFacade bookingFacade;
@@ -27,5 +41,34 @@ public class UserController {
 		model.addAttribute("user", bookingFacade.getUserById(userId));
 		model.addAttribute("userAccount", bookingFacade.getUserAccountById(userId));
 		return "userDetails";
+	}
+	
+	@RequestMapping(value = "/user/create",  method = RequestMethod.GET)
+	public String createUser() {
+		return "createUser";
+	}
+	
+	@RequestMapping(value = "/user/create", method = RequestMethod.POST)
+	@Transactional
+	public String submitUser(@ModelAttribute("name")String name,
+							@ModelAttribute("email")String email,
+							@ModelAttribute("amount")String amount, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			result.getFieldErrors().forEach(logger::error);
+			return "createUser";
+		} else {
+			try {
+				logger.info("Input parameters from form: name = {}, email = {}, amount = {}", name, email, amount);
+				User user = new UserImpl();
+				user.setName(name);
+				user.setEmail(email);
+				bookingFacade.createUser(user);
+				bookingFacade.createUserAccount(user, Double.parseDouble(amount));
+				return "redirect:/users/user/id/" + user.getId();
+			} catch (Exception e) {
+				logger.error(e);
+				return "createUser";
+			}
+		}
 	}
 }
---
+++ Spring/WebModule/src/main/webapp/WEB-INF/content/jsp/createUser.jsp
@@ -0,0 +1,30 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<html>
+	<head>
+	    <title>Ticket booking</title>
+	</head>
+	<body>
+	     <h3>Create a new user</h3>
+	     <form method="post">
+	        <table>
+        		<tr>
+            		<td>Name:</td>
+           			<td><input name="name" value="Leonid Brezhnev"/></td>
+        		</tr>
+       			<tr>
+            		<td>Email:</td>
+            		<td><input path="email" value="leonidillich@kpss.su"/></td>
+        		</tr>
+        		<tr>
+            		<td>Amount:</td>
+            		<td><input path="amount" value="0.0"/></td>
+        		</tr>
+        		<tr>
+            		<td colspan="2">
+                		<input type="submit" value="Save Changes" />
+            		</td>
+        		</tr>
+    		</table>
+	     </form>
+	</body>
+</html>
